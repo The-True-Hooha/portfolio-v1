8de71425adbdf60e12b2bd200c2364d7d04f6d93
David Ogar <owogogahhero@outlook.com>
2022-06-30 16:25:36 +0100

    feat: add hero section

---
+++ components/Hero.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const HeroSection = () => {
+    return(
+        <div className='w-full h-screen px-[300px]'>
+            <div className='max-w[1240px] w-full h-full mx-auto py-[200px] justify-center place-items-start'>
+                <div>
+                    <h2 className='text-white text-lg font-mono my-[-18h2x]'> Hello, my name is</h2>
+                    <h2 className='font-calibre'>David Ogar</h2>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default HeroSection;
