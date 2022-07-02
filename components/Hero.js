bc7b84329dfa579a48bd8769fa9ef3d7b0e98a6d
David Ogar <owogogahhero@outlook.com>
2022-07-02 15:13:58 +0100

    refactor: fix responsiveness

--- components/Hero.js
+++ components/Hero.js
@@ -3,26 +3,31 @@ import Link from "next/link";
 
 const HeroSection = () => {
   return (
-    <div className="py-[180px] px-[60px] sm:px-[60px] lg:px-[290px] flex-auto sm:flex-auto lg:flex-auto">
+    <div className="py-[180px] px-[60px] text-left sm:px-[60px] lg:px-[290px] flex-auto sm:flex-auto lg:flex-auto">
       <div className="">
-      <h1 className="font-SFMono text-[16px] sm:text-[20px]">Hello, my name is</h1>
-      <h2 className="text-white font-calibre text-[40px] sm:text-[70px] sm:mt-[20px] font-bold mt-[8px]">
-        David Ogar.
-      </h2>
-      <h2 className="font-calibre text-[25px] font-semibold sm:text-[45px] sm:mt-[35px] mt-[15px]">
-      A Software Engineer
-      </h2>
-      <p className="mt-[5px] max-w-[400px] text-[15px] sm:max-w-[600px] sm:mt-[10px] text-white">
-        with specialization in developing{" "}
-        <span className="text-[#658e8d] pr-1">server-side applications</span>
-        to create <span className="text-[#658e8d]">scalable technological solutions</span> and propel
-        innovation. I spend my free time learning about reverse and autonomous engineering.
-      </p>
+        <h1 className="font-SFMono text-[16px] sm:text-[20px]">
+          Hello, my name is
+        </h1>
+        <h2 className="text-white font-calibre text-[40px] sm:text-[70px] sm:mt-[20px] font-bold mt-[8px]">
+          David Ogar.
+        </h2>
+        <h2 className="font-calibre text-[25px] font-semibold sm:text-[45px] sm:mt-[35px] mt-[15px]">
+          A Software Engineer
+        </h2>
+        <p className="mt-[5px] max-w-[400px] text-[15px] sm:max-w-[600px] sm:mt-[10px] text-white">
+          with specialization in developing server-side applications. I spend my
+          free time learning about reverse and autonomous engineering.
+        </p>
       </div>
-      <div className="font-SFMono text-[20px] text-white sm:text-[25px] mt-6 lg:mt-[10px]">
-      <Link href="/">
-        <a target="_blank" className="border hover:bg-[#9db03c] px-3 py-2 text-sm border-white rounded">Resume</a>
-      </Link>
+      <div className="font-SFMono text-[20px] text-white sm:text-[25px] lg:mt-[30px] mt-6">
+        <Link href="/">
+          <a
+            target="_blank"
+            className="border hover:bg-[#9db03c] px-3 py-2 text-sm border-white rounded"
+          >
+            Resume
+          </a>
+        </Link>
       </div>
     </div>
   );
