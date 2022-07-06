40f4c3d79ded421160741e52cbb0036cf1c823b2
David Ogar <owogogahhero@outlook.com>
2022-07-06 14:58:55 +0100

    fix styling

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -1,12 +1,16 @@
-const ProjectCard = ({icon, title, projectDescription}) => {
+const ProjectCard = ({icon, title, projectDescription, github, stack}) => {
   return (
-    <div className="h-[300px] text-white w-[300px] bg-[#1b1b1d] cursor-pointer hover:mt-[-8px]">
+    <div className="h-[300px] drop-shadow-xl hover:text-[#617278] text-white w-[300px] duration-300 bg-[#1b1b1d] cursor-pointer hover:mt-[-8px]">
       <div className="pl-6 pt-6">
-        {icon}
-        <h2 className="text-[20px] text-[#617278] hover:text-white font-semibold font-calibre mt-4">
+        <div className="flex justify-between">
+        {icon} {github}
+        </div>
+        
+        <h2 className="text-[20px] font-semibold font-calibre mt-4">
          {title}
         </h2>
-        <p>{projectDescription}</p>
+        <p className="text-white text-[15px] font-calibre text-justify font-normal">{projectDescription}</p>
+        <p className="absolute mb-9 text-[12px] opacity-50 bottom-0 font-SFMono font-light"> {stack}</p>
       </div>
     </div>
   );
--- components/projects.js
+++ components/projects.js
@@ -1,70 +1,80 @@
 import React from "react";
 import Link from "next/link";
+import ProjectCard from "./ProjectCard";
+import { BsFolderPlus } from "react-icons/bs";
+import { AiOutlineGithub } from "react-icons/ai";
 
 const myProject = [
   {
     title: "Calculator",
     projectDescription: "a calculator app  built from scratch",
-    github: "", //can be null
-    techStack: "Java + Java + AWS + Java",
-    live: "",
+    github: <AiOutlineGithub size={25} className="mr-6" />,
+    stack: "java angular react redis next.js",
+    icon: <BsFolderPlus size={25} className="text-white" />,
   },
   {
     title: "ATM Machine",
     projectDescription: "a calculator app  built from scratch",
-    github: "", //can be null
-    techStack: "Java + Java + AWS + Java",
-    live: "",
+    github: <AiOutlineGithub size={25} className="mr-6" />,
+    stack: "java angular react redis next.js",
+    icon: <BsFolderPlus size={25} className="text-white" />,
+    stack: "",
   },
   {
     title: "Computer Disc",
     projectDescription: "a calculator app  built from scratch",
-    github: "", //can be null
-    techStack: "Java + Java + AWS + Java",
+    github: <AiOutlineGithub size={25} className="mr-6" />,
+    stack: "java angular react redis next.js",
     live: "",
+    icon: <BsFolderPlus size={25} className="text-white" />,
   },
+  {
+    title: "Calculator",
+    projectDescription: "a calculator app  built from scratch",
+    github: <AiOutlineGithub size={25} className="mr-6" />,
+    stack: "java angular react redis next.js",
+    icon: <BsFolderPlus size={25} className="text-white" />,
+  },
+  {
+    title: "ATM Machine",
+    projectDescription: "a calculator app  built from scratch",
+    github: <AiOutlineGithub size={25} className="mr-6" />,
+    stack: "java angular react redis next.js",
+    live: "",
+    icon: <BsFolderPlus size={25} className="text-white" />,
+  },
+  {
+    title: "Computer Disc",
+    projectDescription: "a calculator app  built from scratch",
+    github: <AiOutlineGithub size={25} className="mr-6" />,
+    stack: "java angular react redis next.js",
+    live: "",
+    icon: <BsFolderPlus size={25} className="text-white" />,
+  }
 ];
 
 const Projects = () => {
   return (
-    <div id="projects" className="px-[60px] lg:py-[2px] w-full text-left lg:px-[290px]">
+    <div
+      id="projects"
+      className="px-[60px] lg:py-[2px] w-full text-left lg:px-[200px]"
+    >
       <div>
-        <h2 className="text-[23px] font-calibre font-semibold mt-[-60px] tracking-wide sm:text-[32px] text-left text-white lg:font-bold ">
+        <h2 className="text-[23px] font-calibre font-semibold mt-[-60px] text-left text-white lg:font-bold">
           <span className="font-SFMono text-white text-[20px] font-normal mr-1 text-sm">
             03.
           </span>
           Projects
         </h2>
-        <div className="font-SFMono  flex flex-center w-full lg:mt-[4px] ">
+        <div className="font-SFMono  flex w-full lg:mt-[4px] ">
           <p className="px-7 text-center justify-center text-[14px] hover:underline font-bold mt-1">
             and other noteworthy contributions...
           </p>
         </div>
-        <div className="w-full flex flex-wrap text-justify justify-center gap-[12px] mt-6">
-          <div className="h-[300px] w-[300px] bg-[#1b1b1d] cursor-pointer transition ease-in-out duration-500 hover:scale-[120%]">
-            <h2 className="text-white text-[20px]"> Project Name</h2>
-            <p>lorem ipsum dolor </p>
-          </div>
-          <div className="h-[300px] w-[300px] bg-[#1b1b1d] cursor-pointer transition ease-in-out duration-500 hover:scale-[120%]">
-            <h2 className="text-white text-[20px]"> Project Name</h2>
-            <p>lorem ipsum dolor </p>
-          </div>
-          <div className="h-[300px] w-[300px] bg-[#1b1b1d] cursor-pointer transition ease-in-out duration-500 hover:scale-[120%]">
-            <h2 className="text-white text-[20px]"> Project Name</h2>
-            <p>lorem ipsum dolor </p>
-          </div>
-          <div className="h-[300px] w-[300px] bg-[#1b1b1d] cursor-pointer transition ease-in-out duration-500 hover:scale-[120%]">
-            <h2 className="text-white text-[20px]"> Project Name</h2>
-            <p>lorem ipsum dolor </p>
-          </div>
-          <div className="h-[300px] w-[300px] bg-[#1b1b1d] cursor-pointer transition ease-in-out duration-500 hover:scale-[120%]">
-            <h2 className="text-white text-[20px]"> Project Name</h2>
-            <p>lorem ipsum dolor </p>
-          </div>
-          <div className="h-[300px] w-[300px] bg-[#1b1b1d] cursor-pointer transition ease-in-out duration-500 hover:scale-[120%]">
-            <h2 className="text-white text-[20px]"> Project Name</h2>
-            <p>lorem ipsum dolor </p>
-          </div>
+        <div className="w-full flex flex-wrap text-justify justify-center  mt-6 gap-[12px]">
+          {myProject.map((v, index) => (
+            <ProjectCard {...v} key={index} title={v.title} />
+          ))}
         </div>
       </div>
       <div className="font-SFMono text-[20px] mt-10 font-semibold justify-center flex text-white lg:mt-[60px]">
