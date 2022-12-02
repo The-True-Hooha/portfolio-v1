d0a75edd2cacb05f7dcae86d2ed2e3519c13e074
David Ogar <owogogahhero@outlook.com>
2022-12-02 16:24:53 +0100

    refactor: style: responsiveness

--- components/work.js
+++ components/work.js
@@ -135,7 +135,7 @@ const Work = () => {
     setExpIndex(idx);
   };
   return (
-    <div id="work" className="w-full px-[60px] lg:px-[150px] py-[115px]">
+    <div id="work" className="px-[60px] lg:px-[150px] py-[115px] w-full">
       <div className="w-6/12 mb-[10px] flex flex-row items-center">
         <div className="mr-[12px]">
           {" "}
@@ -148,8 +148,8 @@ const Work = () => {
         </div>
         <div className=" mt-[10px] h-[1px] w-full bg-white"></div>
       </div>
-      <div className="w-full md:w-8/12 mt-[35px] flex flex-col md:flex-row items-start min-h-[300px]">
-        <ul className="w-full md:w-3/12 flex flex-row md:flex-col border-l-[1px] border-solid lg:border-l-2 border-[#1b1b1d]">
+      <div className="md:w-8/12 mt-[35px] flex flex-col md:flex-row items-start min-h-[300px]">
+        <ul className="w-full md:w-3/12 overflow-auto flex flex-row md:flex-col border-l-[1px] border-solid lg:border-l-2 border-[#1b1b1d]">
           {myExperienceData.map(({ id, company }, index) => (
             <li
               onClick={() => handleTabChange(index)}
