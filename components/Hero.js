88bfa46fd930a9c558aea8f82c856748c483af9a
David Ogar <owogogahhero@outlook.com>
2022-12-02 16:12:05 +0100

    refactor: for sm screens

--- components/Hero.js
+++ components/Hero.js
@@ -3,16 +3,14 @@ import Link from "next/link";
 // import Image from "next/image";
 const HeroSection = () => {
   return (
-    <div>
     <div
       id="home"
-      className="w-full px-[60px] py-[120px] lg:px-[150px] md:py-[200px]"
+      className="px-[50px] py-[100px] w-full lg:px-[150px] md:py-[200px]"
     >
       
-        <div className="text-justify">
           <h2 className="font-SFMono text-[16px] ">Hello, my name is</h2>
-          <h1 className="text-white font-calibre lg:text-[50px] text-[38px] font-bold mt-[8px]">
-            David Ogar.
+          <h1 className="text-white font-calibre lg:text-[50px] text-[33px] font-bold mt-[8px]">
+            David Ogar
           </h1>
           <h2 className="font-calibre text-[20px] lg:text-[28px] font-semibold mt-[15px]">
             A Software Engineer
@@ -21,8 +19,8 @@ const HeroSection = () => {
             with specialization in developing server-side applications. I spend
             my free time learning about reverse and autonomous engineering.
           </p>
-        </div>
-        <div className="font-SFMono text-[20px] text-white md:text-[25px] lg:mt-[30px] mt-10">
+        
+        {/* <div className="font-SFMono text-[20px] text-white md:text-[25px] lg:mt-[30px] mt-10">
           <Link href="https://drive.google.com/file/d/1TYUoQ9suUB8GuZbtSWVsaqcm5JD4ljoJ/view?usp=sharing">
             <a
               target="_blank"
@@ -31,8 +29,7 @@ const HeroSection = () => {
               Resume
             </a>
           </Link>
-        </div>
-    </div>
+        </div> */}
     </div>
   );
 };
